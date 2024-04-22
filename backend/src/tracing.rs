4f689196e64fc5b88ca9524cf66dc6210a208daa
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2024-04-22 22:39:25 +0200

    finally managed to get w3c trace context

--- backend/src/tracing.rs
+++ backend/src/tracing.rs
@@ -1,47 +1,67 @@
 use std::time::Duration;
 
-use opentelemetry::KeyValue;
+use opentelemetry::{propagation::TextMapCompositePropagator, KeyValue};
+use opentelemetry_otlp::WithExportConfig;
 use opentelemetry_sdk::{
+    propagation::{BaggagePropagator, TraceContextPropagator},
     resource::{EnvResourceDetector, SdkProvidedResourceDetector, TelemetryResourceDetector},
-    runtime, Resource,
+    trace::{Sampler, Tracer},
+    Resource,
 };
-use tracing_subscriber::{Registry, prelude::__tracing_subscriber_SubscriberExt};
+use tracing::Subscriber;
+use tracing_opentelemetry::OpenTelemetryLayer;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, registry::LookupSpan};
 
-fn init_otel_resource() -> Resource {
-    let otlp_resource_detected = Resource::from_detectors(
+pub fn build_otel_layer<S>() -> color_eyre::Result<OpenTelemetryLayer<S, Tracer>>
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+{
+    let otel_rsrc = Resource::from_detectors(
         Duration::from_secs(3),
         vec![
             Box::new(SdkProvidedResourceDetector),
             Box::new(EnvResourceDetector::new()),
             Box::new(TelemetryResourceDetector),
         ],
-    );
-    let otlp_resource_override = Resource::new(vec![KeyValue::new(
-        opentelemetry_semantic_conventions::resource::SERVICE_NAME,
-        "backend",
-    )]);
-    otlp_resource_detected.merge(&otlp_resource_override)
+    )
+    .merge(&mut Resource::new(vec![
+        KeyValue::new(
+            opentelemetry_semantic_conventions::resource::SERVICE_NAME,
+            "backend",
+        ),
+        KeyValue::new(
+            opentelemetry_semantic_conventions::resource::SERVICE_VERSION,
+            "0.0.0",
+        ),
+    ]));
+
+    let composite_propagator = TextMapCompositePropagator::new(vec![
+        Box::new(TraceContextPropagator::new()),
+        Box::new(BaggagePropagator::new()),
+    ]);
+
+    opentelemetry::global::set_text_map_propagator(composite_propagator);
+    let exporter = opentelemetry_otlp::new_exporter()
+        .tonic()
+        .with_endpoint("grpc://localhost:4317");
+
+    Ok(tracing_opentelemetry::layer()
+        .with_error_records_to_exceptions(true)
+        .with_tracer(
+            opentelemetry_otlp::new_pipeline()
+                .tracing()
+                .with_exporter(exporter)
+                .with_trace_config(
+                    opentelemetry_sdk::trace::config()
+                        .with_resource(otel_rsrc)
+                        .with_sampler(Sampler::AlwaysOn),
+                )
+                .install_batch(opentelemetry_sdk::runtime::Tokio)?,
+        ))
 }
+
 pub fn init_tracing() -> color_eyre::Result<()> {
-    let metrics_pipeline = opentelemetry_otlp::new_pipeline()
-        .metrics(opentelemetry_sdk::runtime::Tokio)
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
-        .with_resource(init_otel_resource())
-        .build()?;
-
-    let metrics_subscriber_layer = tracing_opentelemetry::MetricsLayer::new(metrics_pipeline);
-
-    let tracer = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(opentelemetry_otlp::new_exporter().tonic())
-        .with_trace_config(opentelemetry_sdk::trace::config().with_resource(init_otel_resource()))
-        .install_batch(runtime::Tokio)?;
-    let tracing_layer = tracing_opentelemetry::layer().with_tracer(tracer);
-
-    let telemetry_subscriber = Registry::default()
-        .with(metrics_subscriber_layer)
-        .with(tracing_layer);
-
-    tracing::subscriber::set_global_default(telemetry_subscriber)?;
+    let subscriber = tracing_subscriber::registry().with(build_otel_layer()?);
+    tracing::subscriber::set_global_default(subscriber)?;
     Ok(())
 }
